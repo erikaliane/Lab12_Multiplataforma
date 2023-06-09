4e3fba423d5126066e93cee85665b2264bbd6aa8
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-06-08 23:24:50 -0500

    Agregar archivos de proyecto.

---
+++ Lab12_Multiplataforma/Lab12_Multiplataforma/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab12_Multiplataforma.ViewModel
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private string _result;
+
+        public CalculatorViewModel()
+        {
+            CalculateCommand = new Command<string>(Calculate);
+            ClearCommand = new Command(Clear);
+            _result = string.Empty;
+        }
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public ICommand CalculateCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        private void Calculate(string parameter)
+        {
+            if (parameter == "=")
+            {
+                Result = new DataTable().Compute(Result, null).ToString();
+            }
+            else
+            {
+                Result += parameter;
+            }
+        }
+
+        private void Clear()
+        {
+            Result = string.Empty;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
